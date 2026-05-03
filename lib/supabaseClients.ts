9b9ede33b71d25803d3f57ade2512ed1f84ec4de
rsfaell <rafaguitrab@gmail.com>
2026-05-03 12:51:25 -0300

    Tudo

--- lib/supabaseClients.ts
+++ lib/supabaseClients.ts
@@ -1,11 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: true, // Isso deve estar como TRUE
-    autoRefreshToken: true,
-  }
-})
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.warn("Atenção: Variáveis do Supabase não encontradas.")
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export default supabase
