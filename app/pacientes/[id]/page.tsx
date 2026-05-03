170e92ba2f39b30bfa898e30a6cf4c33b99efa4c
rsfaell <rafaguitrab@gmail.com>
2026-05-03 16:15:22 -0300

    Ajuste

--- app/pacientes/[id]/page.tsx
+++ app/pacientes/[id]/page.tsx
@@ -1,12 +1,10 @@
-import ProntuarioPacienteClient from '@/components/ProntuarioPacienteClient'
+import ProntuarioPacienteClient from '@/components/ProntuarioPacienteClient';
 
-export const dynamicParams = false;
-
-export async function generateStaticParams() {
-  return [{ id: '1' }];
-}
-
-export default async function ProntuarioPaciente({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  return <ProntuarioPacienteClient id={id} />
+export default async function ProntuarioPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const resolvedParams = await params;
+  return <ProntuarioPacienteClient id={resolvedParams.id} />;
 }
